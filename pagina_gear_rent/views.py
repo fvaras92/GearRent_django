dc0659e7d44bca863e00c70670bd2db73f1aaf6b
fvaras92 <106790096+fvaras92@users.noreply.github.com>
2023-06-10 11:58:06 -0400

    templates
    creacion de product_list y cuerpo

--- pagina_gear_rent/views.py
+++ pagina_gear_rent/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+
+
+def product_list(request):
+    products = Product.objects.all
+    return render(request, 'tienda/product_list.html', {'products': products})
