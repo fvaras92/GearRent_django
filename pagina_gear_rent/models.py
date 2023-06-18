a966da506e0b3bbd92963f1834dbb912422c7fb8
fvaras92 <106790096+fvaras92@users.noreply.github.com>
2023-06-17 21:31:34 -0400

    agrego producto

--- pagina_gear_rent/models.py
+++ pagina_gear_rent/models.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     codigo = models.CharField(max_length=100)
     nombre = models.CharField(max_length=100)
     marca = models.CharField(max_length=100)
-    imagen= models.ImageField(upload_to='pagina_gear_rent/static/img' ,blank=True, null= True)
+    imagen= models.ImageField(blank=True, null= True)
     descripcion = models.TextField()
     precio = models.IntegerField()
     stock = models.IntegerField()
