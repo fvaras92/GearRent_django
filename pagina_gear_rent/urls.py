787b60ccee3183f0909a38ef766889b264f8839b
fvaras92 <106790096+fvaras92@users.noreply.github.com>
2023-06-17 21:24:21 -0400

    otro comit

--- pagina_gear_rent/urls.py
+++ pagina_gear_rent/urls.py
@@ -5,4 +5,4 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('tienda', views.product_list, name='product_list'),
-] 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
