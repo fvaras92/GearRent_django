6e5e9b5b399a057c2edb9a0ba8a9303721c6d637
dead doomed <igna.gallardo@duocuc.cl>
2023-06-24 04:50:22 -0400

    links funcionando

--- pagina_gear_rent/static/js/cat_fact.js
+++ pagina_gear_rent/static/js/cat_fact.js
@@ -1,13 +1,20 @@
 function boton(){
     for(var i = 0; i < 20; i++){
-        cat_fact() 
+        if(cat_fact()==true){
+            break;
+        }
     }
 }
 
 function cat_fact(){
     $.get("https://cat-fact.herokuapp.com/facts/random", function(data, status){
+        console.log(data.status.verified);
         if(data.status.verified){
             document.getElementById("cat_fact").innerHTML=data.text;
+            return true;
+        }
+        else{
+            return false;
         }
     })
 }
